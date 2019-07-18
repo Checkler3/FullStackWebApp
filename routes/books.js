6776f6918d713094fef7e6ea70967d3e2a37f8d5
Corey Heckler <50090414+Checkler3@users.noreply.github.com>
2019-07-18 14:39:04 -0400

    Add backend functionalilty for books

--- routes/books.js
+++ routes/books.js
@@ -1,7 +1,17 @@
 const express = require('express');
 const router = express.Router();
+const multer = require('multer');
+const path = require('path');
 const Book = require('../models/book');
+const uploadPath = path.join('public', Book.coverImageBasePath);
 const Authors = require('../models/author');
+const imageMineTypes = ['image/jpeg', 'image/png', 'image/gif'];
+const upload = multer({
+	dest: uploadPath,
+	fileFilter: (req, file, callback) => {
+		callback(null, imageMineTypes.includes(file.mimetype));
+	}
+});
 
 // All Books Route
 router.get('/', async (req, res) => {
@@ -10,21 +20,45 @@ router.get('/', async (req, res) => {
 
 // New Book Route
 router.get('/new', async (req, res) => {
-	try {
-		const authors = await Authors.find({});
-		const book = new Book();
-		res.render('books/new', {
-			authors: authors,
-			book: book
-		});
-	} catch {
-		res.redirect('/books');
-	}
+	renderNewPage(res, new Book());
 });
 
 // Create Book Route
-router.post('/', async (req, res) => {
-	res.send('Create Books');
+router.post('/', upload.single('cover'), async (req, res) => {
+	const fileName = req.file != null ? req.file.fieldname : null;
+	const book = new Book({
+		title: req.body.title,
+		author: req.body.author,
+		publishDate: new Date(req.body.publishDate),
+		pageCount: req.body.pageCount,
+		coverImageName: fileName,
+		description: req.body.description
+	});
+
+	try {
+		const newBook = await book.save();
+		//res.redirect(`books/${newBook.id}`)
+		res.redirect('books');
+	} catch {
+		// if (book.coverImageName != null) {
+		// 	removeBookCover(book.coverImageName);
+		// }
+		renderNewPage(res, book, true);
+	}
 });
+
+async function renderNewPage(res, book, hasError = false) {
+	try {
+		const authors = await Authors.find({});
+		const params = {
+			authors: authors,
+			book: book
+		};
+		if (hasError) params.errorMessage = 'Error Creating Book';
+		res.render('books/new', params);
+	} catch {
+		res.redirect('/books');
+	}
+}
 
 module.exports = router;
