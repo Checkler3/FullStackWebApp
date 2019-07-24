99b487db3d8e9002f345f3bd9ab3c2fd62feb547
Corey Heckler <50090414+Checkler3@users.noreply.github.com>
2019-07-24 11:19:12 -0400

    Bug Fixed!!
    Books.js changes:
    changed this.coverImageName (which was invalid) to this.coverImage.
    
    Apparently that is all that needed to change for it to work.

--- models/book.js
+++ models/book.js
@@ -40,9 +40,10 @@ const bookSchema = new mongoose.Schema({
 });
 
 bookSchema.virtual('coverImagePath').get(function() {
-	if (this.coverImageName != null && this.coverImageType != null) {
-		return `data: ${this.coverImageType};charset=utf-8;base64,
-		${this.coverImage.toString('base64')}`;
+	if (this.coverImage != null && this.coverImageType != null) {
+		return `data:${
+			this.coverImageType
+		};charset=utf-8;base64,${this.coverImage.toString('base64')}`;
 	}
 });
 
