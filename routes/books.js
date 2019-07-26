bd7fd9c6a0ff17221e271e7a40b7202ad6c13d3e
Corey Heckler <50090414+Checkler3@users.noreply.github.com>
2019-07-26 12:50:34 -0400

    Finished Backend

--- routes/books.js
+++ routes/books.js
@@ -45,8 +45,7 @@ router.post('/', async (req, res) => {
 
 	try {
 		const newBook = await book.save();
-		// res.redirect(`books/${newBook.id}`)
-		res.redirect(`books`);
+		res.redirect(`books/${newBook.id}`);
 	} catch {
 		renderNewPage(res, book, true);
 	}
@@ -74,6 +73,52 @@ router.get('/:id/edit', async (req, res) => {
 	}
 });
 
+// Update Book Route
+router.put('/:id', async (req, res) => {
+	let book;
+
+	try {
+		book = await Book.findById(req.params.id);
+		book.title = req.body.title;
+		book.title = req.body.title;
+		book.author = req.body.author;
+		book.publishDate = new Date(req.body.publishDate);
+		book.pageCount = req.body.pageCount;
+		book.description = req.body.description;
+
+		if (req.body.cover != null && req.body.cover != '') {
+			saveCovwer(book, req.body.cover);
+		}
+		await book.save();
+		res.redirect(`/books/${book.id}`);
+	} catch {
+		if (book != null) {
+			renderNewPage(res, book, true);
+		} else {
+			res.redirect('/');
+		}
+	}
+});
+
+// Delete Book Route
+router.delete('/:id', async (req, res) => {
+	let book;
+	try {
+		book = await Book.findById(req.params.id);
+		await book.remove();
+		res.redirect('/books');
+	} catch (e) {
+		if (book != null) {
+			res.render('books/show', {
+				book: book,
+				errorMessage: 'Could not remove book'
+			});
+		} else {
+			res.redirect('/');
+		}
+	}
+});
+
 async function renderNewPage(res, book, hasError = false) {
 	renderFormPage(res, book, 'new', hasError);
 }
@@ -89,7 +134,13 @@ async function renderFormPage(res, book, form, hasError = false) {
 			authors: authors,
 			book: book
 		};
-		if (hasError) params.errorMessage = 'Error Creating Book';
+		if (hasError) {
+			if (form === 'edit') {
+				if (hasError) params.errorMessage = 'Error Editing Book';
+			} else {
+				if (hasError) params.errorMessage = 'Error Creating Book';
+			}
+		}
 		res.render(`books/${form}`, params);
 	} catch {
 		res.redirect('/books');
